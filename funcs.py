a6cfeaf0d61e85044b79f3c13c3943fe6d70fc60
keouck <imerminu@tutanota.com>
2022-10-02 14:46:52 +0530

    Add webpages

--- funcs.py
+++ funcs.py
@@ -1,12 +1,12 @@
 import requests
 import json
 
-def get_apod_data(api_key):
-    raw_response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={api_key}').text
+def get_apod_data(api_key, date):
+    raw_response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={api_key}&date').text
     response = json.loads(raw_response)
     return response
 
-def get_neows_data(start_date, end_date, api_key):
-    raw_response = requests.get(f'https://api.nasa.gov/neo/rest/v1/feed?start_date={start_date}&end_date={end_date}&api_key={api_key}').text
+def get_image(query):
+    raw_response = requests.get(f'https://images-api.nasa.gov/search?q={query}')
     response = json.loads(raw_response)
     return response
